474bc1865e7d3fa2aba925101e304e30181825b3
Itallo <italloalexsander@gmail.com>
2022-05-19 18:12:59 -0300

    Remove commented code

--- scripts.js
+++ scripts.js
@@ -47,38 +47,6 @@ const gamePlay = (userSelection) =>{
     }
 }
 
-//This function will play a 5 round game of RPS
-/*let game = () =>{
-
-    totalWinsUser = 0;
-    totalWinsComputer = 0;
-
-    for(let i = 0; i < 5; i++){
-        let userSelection = prompt("Rock, Paper or Scissors? ")
-        userSelection = userSelection.toLowerCase();
-        let computerSelection = computerPlay();
-        let result = gamePlay(userSelection, computerSelection)
-        if(result === "Draw"){
-            console.log("Draw! Both you and the computer have chosen " + userSelection);
-        }else if(result === "ComputerWins"){
-            console.log("You Lost! The computer " + computerSelection + " beats your " + userSelection);
-            totalWinsComputer++;
-        }
-        else if(result === "UserWins"){
-            console.log("You WON! Your " + userSelection + " beats the computer " + computerSelection)
-            totalWinsUser++;
-        }
-    }
-
-    if(totalWinsUser > totalWinsComputer){
-        console.log("YOU WON IT ALL!");
-    }else if(totalWinsUser < totalWinsComputer){
-        console.log("YOU LOST IT ALL!");
-    }else{
-        console.log("IT'S A DRAW");
-    }
-}*/
-
 const score = [];
 score[0] = 0; //User score
 score[1] = 0; //Computer Score
@@ -88,13 +56,38 @@ const userScoreText = document.getElementById('userScore')
 const computerScoreText = document.getElementById('computerScore')
 const drawsText = document.getElementById('draws')
 
+
+
+const resetScoreHandler = () => {
+    score[0] = 0;
+    score[1] = 0;
+    score[2] = 0;
+
+    userScoreText.textContent = `${score[0]}`;
+    computerScoreText.textContent = `${score[1]}`;
+    drawsText.textContent = `${score[2]}`;
+}
+
+
+const winnerHandler = (winner) =>{
+ if(winner === "User-Wins"){
+     alert("Congratulations, you won!");
+     resetScoreHandler();
+     
+ }
+ if(winner === "Computer-Wins"){
+     alert("The computer won!");
+     resetScoreHandler();
+ }
+}
+
 const updateScoreHandler = result =>{
     if(result === "UserWins"){
-        score[0]++;
+        ++score[0];
         userScoreText.textContent = `${score[0]}`;
     }
     else if(result === "ComputerWins"){
-        score[1]++;
+        ++score[1];
         computerScoreText.textContent = `${score[1]}`;
     }
     else{
@@ -103,10 +96,12 @@ const updateScoreHandler = result =>{
     }
 
     if(score[0] === 5){
-        console.log("User-Wins")
+        userScoreText.textContent = `${score[0]}`;
+        winnerHandler("User-Wins");
     }
     else if(score[1] === 5){
-        console.log("Computer-Wins")
+        computerScoreText.textContent = `${score[1]}`;
+        winnerHandler("Computer-Wins");
     }
 
     return "Keep-Going";
